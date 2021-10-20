e983bc8fc56fc7ee396c5297c6f781f99a8446fe
Katja <katja.runge@jimdo.com>
2021-10-20 23:53:11 +0200

    fix: add babel config

--- __test__/app.spec.js
+++ __test__/app.spec.js
@@ -1,4 +1,4 @@
-import { getCityName } from '../src/client/js/app';
+import { getCityName } from '../src/client/js/app.js';
 
 test('getCityName() must be defined', () => {
   expect(getCityName).toBeDefined();
